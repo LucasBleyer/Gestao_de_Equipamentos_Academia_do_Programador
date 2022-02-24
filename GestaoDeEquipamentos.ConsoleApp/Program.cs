fc803bb1905ab35c95bf835ce822a1b2e58cd667
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-24 17:12:44 -0300

    opcoes do menu

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -6,7 +6,29 @@ namespace GestaoDeEquipamentos.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            do
+            {
+                MenuOpcoes();
+            } while (true);
+            
         }
+
+    #region Métodos do Menu
+        static void MenuOpcoes()
+        {
+            Console.Write("[1] Registrar equipamentos: ");
+            Console.Write("[2] Visualizar todos os equipamentos registrados: ");
+            Console.Write("[3] Editar um equipamento: ");
+            Console.Write("[4] Excluir um equipamento já registrado: ");
+            Console.Write("[5] Excluir um equipamento já registrado: ");
+        }
+
+        #endregion
+
+    #region Métodos de Controle
+        #endregion
+
+    #region Métodos de Chamado
+        #endregion
     }
 }
