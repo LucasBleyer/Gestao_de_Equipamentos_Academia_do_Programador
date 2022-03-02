0a4a9dedfb18d9bdd7c2f6e966f74f108820f056
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-03-02 15:55:48 -0300

    add menu principal e menu secundario

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -11,13 +11,91 @@ namespace GestaoDeEquipamentos.ConsoleApp
             int[] numero_serie = new int[1000];
             String[] data_fabricacao = new string[1000];
             String[] nome_fabricante = new string[1000];
-            char opcao;
 
-            MenuOpcoes(out opcao);
+            String[] titulo_chamado = new string[1000];
+            String[] descricao_chamado = new String[1000];
+            String[] equipamento_chamado = new String[1000];
+            String[] data_abertura_chamado = new string[1000];
 
-            switch (opcao)
+            while (true)
             {
-                case '1': 
+                InserirTituloLimpar("Manutenção de Equipamentos");
+                char opcao_menu_principal = MenuPrincipal();
+
+                if (opcao_menu_principal == '1')
+                {
+                    InserirTituloLimpar("Controle de Equipamentos");
+                    char opcao_controle_equipamentos;
+                    MenuOpcoesControleEquipamentos(out opcao_controle_equipamentos);
+                    VerificaOpcaoControleEquipamentos(nome_equipamento, preco_equipamento, numero_serie, data_fabricacao, nome_fabricante, opcao_controle_equipamentos);
+                }
+                else if (opcao_menu_principal == '2')
+                {
+                    InserirTituloLimpar("Controle de Chamados");
+                    char opcao_controle_chamados;
+                    MenuOpcoesControleChamados(out opcao_controle_chamados);
+                    VerificaOpcaoControleChamados(titulo_chamado, descricao_chamado, equipamento_chamado, data_abertura_chamado, opcao_controle_chamados);
+                }
+                else if (opcao_menu_principal == 's')
+                {
+                    break;
+                }
+            }
+        }
+
+        #region Métodos do Menu
+        static char MenuPrincipal()
+        {
+            char opcao_menu_principal;
+            Console.WriteLine("[1] Controle de Equipamentos");
+            Console.WriteLine("[2] Controle de Chamados");
+            Console.WriteLine("[s] Sair");
+            opcao_menu_principal = Convert.ToChar(Console.ReadLine().ToLower());
+            return opcao_menu_principal;
+        }
+        static char MenuOpcoesControleEquipamentos(out char opcao_controle_equipamentos)
+        {
+            Console.WriteLine("[1] Registrar equipamentos");
+            Console.WriteLine("[2] Visualizar todos os equipamentos registrados");
+            Console.WriteLine("[3] Editar um equipamento");
+            Console.WriteLine("[4] Excluir um equipamento já registrado");
+            Console.WriteLine("[s] Sair Controle de Equipamentos");
+            opcao_controle_equipamentos = Convert.ToChar(Console.ReadLine());
+            return opcao_controle_equipamentos;
+        }
+        static char MenuOpcoesControleChamados(out char opcao_controle_chamados)
+        {
+            Console.WriteLine("[1] Título do Chamado");
+            Console.WriteLine("[2] Descrição do Chamado");
+            Console.WriteLine("[3] Equipamento");
+            Console.WriteLine("[4] Data de Abertura");
+            Console.WriteLine("[s] Sair Controle de Chamados");
+            opcao_controle_chamados = Convert.ToChar(Console.ReadLine());
+            return opcao_controle_chamados;
+        }
+        static void VerificaOpcaoControleChamados(string[] titulo_chamado, string[] descricao_chamado, string[] equipamento_chamado, string[] data_abertura_chamado, char opcao_controle_chamados)
+        {
+            switch (opcao_controle_chamados)
+            {
+                case '1':
+                    TituloChamado(titulo_chamado, descricao_chamado, equipamento_chamado, data_abertura_chamado);
+                    break;
+                case '2':
+                    DescricaoChamado(titulo_chamado, descricao_chamado, equipamento_chamado, data_abertura_chamado);
+                    break;
+                case '3':
+                    EquipamentoChamado(titulo_chamado, descricao_chamado, equipamento_chamado, data_abertura_chamado);
+                    break;
+                case '4':
+                    DataAberturaChamado(titulo_chamado, descricao_chamado, equipamento_chamado, data_abertura_chamado);
+                    break;
+            }
+        }
+        static void VerificaOpcaoControleEquipamentos(string[] nome_equipamento, decimal[] preco_equipamento, int[] numero_serie, string[] data_fabricacao, string[] nome_fabricante, char opcao_controle_equipamentos)
+        {
+            switch (opcao_controle_equipamentos)
+            {
+                case '1':
                     RegistrarEquipamento(nome_equipamento, preco_equipamento, numero_serie, data_fabricacao, nome_fabricante);
                     break;
                 case '2':
@@ -32,46 +110,54 @@ namespace GestaoDeEquipamentos.ConsoleApp
             }
         }
 
-        #region Métodos do Menu
-        static char MenuOpcoes(out char opcao)
-        {
-            Console.WriteLine("[1] Registrar equipamentos");
-            Console.WriteLine("[2] Visualizar todos os equipamentos registrados");
-            Console.WriteLine("[3] Editar um equipamento");
-            Console.WriteLine("[4] Excluir um equipamento já registrado");
-            opcao = Convert.ToChar(Console.ReadLine());
-            return opcao;
-        }
         #endregion
 
         #region Métodos de Controle
 
         static void RegistrarEquipamento(string[] nome_equipamento, decimal[] preco_equipamento, int[] numero_serie, string[] data_fabricacao, string[] nome_fabricante)
         {
-            TituloLimpar("Regitrar Equipamento");
-        }  
+            InserirTituloLimpar("Regitrar Equipamento");
+        }
 
         static void VizualizarEquipamento(string[] nome_equipamento, decimal[] preco_equipamento, int[] numero_serie, string[] data_fabricacao, string[] nome_fabricante)
         {
-            TituloLimpar("Vizualizar Equipamento");
+            InserirTituloLimpar("Vizualizar Equipamento");
         }
 
         static void EditarEquipamento(string[] nome_equipamento, decimal[] preco_equipamento, int[] numero_serie, string[] data_fabricacao, string[] nome_fabricante)
         {
-            TituloLimpar("Editar Equipamento");
+            InserirTituloLimpar("Editar Equipamento");
         }
 
         static void ExcluirEquipamento(string[] nome_equipamento, decimal[] preco_equipamento, int[] numero_serie, string[] data_fabricacao, string[] nome_fabricante)
         {
-            TituloLimpar("Excluir Equipamento");
+            InserirTituloLimpar("Excluir Equipamento");
         }
         #endregion
 
         #region Métodos de Chamado
+
+        static void TituloChamado(string[] titulo_chamado, string[] descricao_chamado, string[] equipamento_chamado, string[] data_abertura_chamado)
+        {
+            InserirTituloLimpar("Título do Chamado");
+        }
+        static void DescricaoChamado(string[] titulo_chamado, string[] descricao_chamado, string[] equipamento_chamado, string[] data_abertura_chamado)
+        {
+            InserirTituloLimpar("Descrição do Chamado");
+        }
+        static void EquipamentoChamado(string[] titulo_chamado, string[] descricao_chamado, string[] equipamento_chamado, string[] data_abertura_chamado)
+        {
+            InserirTituloLimpar("Equipamento do Chamado");
+        }
+        static void DataAberturaChamado(string[] titulo_chamado, string[] descricao_chamado, string[] equipamento_chamado, string[] data_abertura_chamado)
+        {
+            InserirTituloLimpar("Data de Abertura Chamado");
+        }
+
         #endregion
 
         #region Métodos Auxiliares
-        static void TituloLimpar(string mensagem)
+        static void InserirTituloLimpar(string mensagem)
         {
             Console.Clear();
             Console.WriteLine(mensagem);
