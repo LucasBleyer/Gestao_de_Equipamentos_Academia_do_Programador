736af5c1c133346440ab233d246cb04864550f9e
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-03-02 14:31:35 -0300

    criacao de metodos de controle

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -7,19 +7,68 @@ namespace GestaoDeEquipamentos.ConsoleApp
         static void Main(string[] args)
         {
             String[] nome_equipamento = new string[1000];
-            String[] preco_equipamento = new string[1000];
+            decimal[] preco_equipamento = new decimal[1000];
             int[] numero_serie = new int[1000];
-            DateTime[] data_fabricacao = new DateTime[1000];
+            String[] data_fabricacao = new string[1000];
             String[] nome_fabricante = new string[1000];
+            char opcao = '1';
+
+            MenuOpcoes(opcao);
+
+            switch (opcao)
+            {
+                case '1': 
+                    RegistrarEquipamento(nome_equipamento, preco_equipamento, numero_serie, data_fabricacao, nome_fabricante);
+                    break;
+                case '2':
+                    VizualizarEquipamento(nome_equipamento, preco_equipamento, numero_serie, data_fabricacao, nome_fabricante);
+                    break;
+                case '3':
+                    EditarEquipamento(nome_equipamento, preco_equipamento, numero_serie, data_fabricacao, nome_fabricante);
+                    break;
+                case '4':
+                    ExcluirEquipamento(nome_equipamento, preco_equipamento, numero_serie, data_fabricacao, nome_fabricante);
+                    break;
+            }
         }
 
-    #region Métodos do Menu
+        #region Métodos do Menu
+        static char MenuOpcoes(char opcao)
+        {
+            Console.WriteLine("[1] Registrar equipamentos");
+            Console.WriteLine("[2] Visualizar todos os equipamentos registrados");
+            Console.WriteLine("[3] Editar um equipamento");
+            Console.WriteLine("[4] Excluir um equipamento já registrado");
+            opcao = Convert.ToChar(Console.ReadLine());
+            return opcao;
+        }
         #endregion
 
-    #region Métodos de Controle
+        #region Métodos de Controle
+
+        static void RegistrarEquipamento(string[] nome_equipamento, decimal[] preco_equipamento, int[] numero_serie, string[] data_fabricacao, string[] nome_fabricante)
+        {
+
+        }
+
+        static void VizualizarEquipamento(string[] nome_equipamento, decimal[] preco_equipamento, int[] numero_serie, string[] data_fabricacao, string[] nome_fabricante)
+        {
+
+        }
+
+        static void EditarEquipamento(string[] nome_equipamento, decimal[] preco_equipamento, int[] numero_serie, string[] data_fabricacao, string[] nome_fabricante)
+        {
+
+        }
+
+        static void ExcluirEquipamento(string[] nome_equipamento, decimal[] preco_equipamento, int[] numero_serie, string[] data_fabricacao, string[] nome_fabricante)
+        {
+
+        }
+
         #endregion
 
-    #region Métodos de Chamado
+        #region Métodos de Chamado
         #endregion
 
     }
